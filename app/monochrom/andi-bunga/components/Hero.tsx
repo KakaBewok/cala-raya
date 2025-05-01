31bcfde0b128b0f1a54a7a4a94bc479ea8fba0a1
noprizal <rizalnov667@gmail.com>
2025-05-02 00:44:45 +0700

    fix main

--- app/monochrom/andi-bunga/components/Hero.tsx
+++ app/monochrom/andi-bunga/components/Hero.tsx
@@ -1,26 +1,59 @@
+'use client";';
+
+import data from "../data/config.json";
+import { useSearchParams } from "next/navigation";
+//
+import { Geist } from "next/font/google";
+
+const geist = Geist({
+  subsets: ["latin"],
+});
+//
+
 export default function Hero() {
-  const handleScroll = () => {
-    const target = document.getElementById("schedule");
-    target?.scrollIntoView({ behavior: "smooth" });
+  const searchParams = useSearchParams();
+  const to = searchParams.get("to");
+  console.log(to);
+
+  const handleOpenInvitation = () => {
+    const el = document.getElementById("schedule");
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   return (
-    <div
-      data-aos="fade-up"
-      id="hero"
-      className="min-h-screen flex items-center justify-center bg-black text-white text-center p-8"
-    >
-      <div>
-        <h1 className="text-5xl mb-4 tracking-wide">Andi & Bunga</h1>
-        <p className="text-xl mb-2">Mengundang Anda ke pernikahan kami</p>
-        <p className="text-sm">Sabtu, 17 Mei 2025</p>
-        <button
-          onClick={handleScroll}
-          className="mt-4 px-6 py-2 border border-white rounded hover:bg-white hover:text-black transition duration-300"
+    <div className="relative overflow-x-hidden">
+      <section className="relative h-screen w-full max-w-sm mx-auto">
+        {/* Background Video */}
+        <video
+          autoPlay
+          muted
+          loop
+          className="absolute inset-0 w-full h-full object-cover"
         >
-          Buka Undangan
-        </button>
-      </div>
+          <source src={data.url_video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-black/70 to-black/95 z-10 pointer-events-none" />
+
+        {/* Content */}
+        <div className="relative z-20 h-full flex flex-col justify-around items-center text-center text-white ">
+          <h1 className="text-5xl font-bold font-serif">AB</h1>
+          <div className="border border-red-500 flex flex-col items-center justify-center">
+            <p className={`text-sm mb-2 ${geist.className}`}>Hello</p>
+            <p className="text-xl mb-6">{to ?? "Family and Friends"}</p>
+            <button
+              onClick={handleOpenInvitation}
+              className="mt-4 px-6 py-2 border border-white hover:bg-white hover:text-black transition duration-300"
+            >
+              OPEN INVITAION
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
