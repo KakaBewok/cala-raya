630f54aa01fe9d5fb7581a9dba5fa3105c67aff1
noprizal <rizalnov667@gmail.com>
2025-05-03 13:34:39 +0700

    fix

--- themes/monochrome/MonochromePage.tsx
+++ themes/monochrome/MonochromePage.tsx
@@ -5,7 +5,6 @@ import "aos/dist/aos.css";
 import { useEffect } from "react";
 import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
-// import Hero from "./components/Hero";
 import RSVPForm from "./components/RSVPForm";
 import Schedule from "./components/Schedule";
 
