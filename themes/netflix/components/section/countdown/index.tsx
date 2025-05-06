10c0491b80c9dd377c0c3e7f14219c7dfd2f279f
noprizal <rizalnov667@gmail.com>
2025-05-07 02:15:00 +0700

    fix

---
+++ themes/netflix/components/section/countdown/index.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useInvitation } from "@/context/InvitationDataContext";
+import { useState, useEffect } from "react";
+
+interface TimeLeft {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+export default function Countdown() {
+  const { invitationData: data } = useInvitation();
+
+  const calculateTimeLeft = (): TimeLeft | null => {
+    const targetDateTime = `${data?.event_date}T00:00:00+07:00`; // WIB timezone
+    const difference = +new Date(targetDateTime) - +new Date();
+
+    if (difference > 0) {
+      return {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    } else {
+      return null;
+    }
+  };
+
+  const [timeLeft, setTimeLeft] = useState<TimeLeft | null>(
+    calculateTimeLeft()
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  if (!timeLeft) {
+    return <p>Acara sudah dimulai!</p>;
+  }
+
+  return (
+    <div className="countdown bg-black text-white mb-14 rounded-lg text-center">
+      <h2
+        className="mb-4 text-lg leading-5 font-bold text-white"
+        data-aos="fade-right"
+      >
+        Countdown ⌛
+      </h2>
+      <div className="flex justify-center gap-5 text-md" data-aos="fade-left">
+        <div className="p-2 bg-neutral-800 rounded-sm w-14 text-center">
+          <p className="text-lg font-medium">{timeLeft?.days}</p>
+          <p className="text-xs text-neutral-500">Hari</p>
+        </div>
+        <div className="p-2 bg-neutral-800 rounded-sm w-14 text-center">
+          <p className="text-lg font-medium">{timeLeft?.hours}</p>
+          <p className="text-xs text-neutral-500">Jam</p>
+        </div>
+        <div className="p-2 bg-neutral-800 rounded-sm w-14 text-center">
+          <p className="text-lg font-medium">{timeLeft?.minutes}</p>
+          <p className="text-xs text-neutral-500">Menit</p>
+        </div>
+        <div className="p-2 bg-neutral-800 rounded-sm w-14 text-center">
+          <p className="text-lg font-medium">{timeLeft?.seconds}</p>
+          <p className="text-xs text-neutral-500">Detik</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- themes/netflix/components/section/detail-info/index.tsx
+++ themes/netflix/components/section/detail-info/index.tsx
@@ -3,7 +3,7 @@
 import { useInvitation } from "@/context/InvitationDataContext";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import SongButton from "../../ui/song-button";
 import BreakingNews from "../breaking-news";
 import Bridegroom from "../bride-groom";
@@ -14,21 +14,69 @@ import LoveStory from "../love-story";
 import OurGallery from "../our-gallery";
 import TitleInfo from "../title-info";
 import WishSection from "../wish";
+import { ChevronsDown, Pause } from "lucide-react";
+// import Countdown from "../countdown";
 
 export default function DetailInfo() {
   const { invitationData: data } = useInvitation();
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" }); // or 'auto'
+  const scrollAnimationId = useRef<number | null>(null);
+  const [isAutoScroll, setIsAutoScroll] = useState<boolean>(false); // status scroll
+  const [showInstruction, setShowInstruction] = useState<boolean>(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => setShowInstruction(false), 4500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
     AOS.init({ duration: 1400, once: true, offset: 300 });
+
     setTimeout(() => {
       AOS.refresh();
     }, 100);
-  }, []);
+
+    const autoScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollPosition = window.innerHeight + window.scrollY;
+
+      if (scrollHeight <= scrollPosition) {
+        // sudah sampai bawah, stop
+        setIsAutoScroll(false);
+        return;
+      }
+
+      window.scrollBy(0, 20);
+      scrollAnimationId.current = requestAnimationFrame(autoScroll);
+    };
+
+    if (isAutoScroll) {
+      autoScroll();
+    }
+
+    return () => {
+      // cleanup saat unmount
+      if (scrollAnimationId.current) {
+        cancelAnimationFrame(scrollAnimationId.current);
+      }
+    };
+  }, [isAutoScroll]); // kalau isAutoScroll berubah, trigger ulang
+
+  const toggleAutoScroll = () => {
+    if (isAutoScroll) {
+      // kalau sedang auto scroll → stop
+      if (scrollAnimationId.current) {
+        cancelAnimationFrame(scrollAnimationId.current);
+      }
+      setIsAutoScroll(false);
+    } else {
+      setIsAutoScroll(true);
+    }
+  };
 
   return (
-    <div className="space-y-5 pt-4 pb-10">
+    <div className="space-y-5 pt-4 pb-10 relative">
       <video
         className="w-full rounded-xs"
         autoPlay
@@ -39,18 +87,86 @@ export default function DetailInfo() {
         <source src={data?.videos?.[0].url} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
       <div className="space-y-3 px-4">
         <TitleInfo />
         <BreakingNews />
         <Bridegroom />
         <LoveStory />
         <OurGallery />
+        {/* <Countdown /> */}
         <Location />
         <Gift />
         <WishSection />
       </div>
       <Footer />
+      <button
+        onClick={toggleAutoScroll}
+        className={`fixed top-4 right-4 z-50 bg-red-600 text-white p-1 cursor-pointer rounded-full shadow opacity-55 hover:opacity-100 transition ${
+          !isAutoScroll ? "animate-bounce" : ""
+        }`}
+      >
+        {isAutoScroll ? <Pause size={13} /> : <ChevronsDown size={13} />}
+      </button>
+      {showInstruction && (
+        <div className="fixed top-15 right-4 z-50 text-white bg-red-500 rounded-sm p-2 text-xs opacity-80">
+          <p>Klik tombol di atas untuk aktifkan Auto Scroll</p>
+        </div>
+      )}
       <SongButton />
     </div>
   );
 }
+
+// export default function DetailInfo() {
+//   const { invitationData: data } = useInvitation();
+
+//   useEffect(() => {
+//     window.scrollTo({ top: 0, behavior: "smooth" }); // or 'auto'
+
+//     AOS.init({ duration: 1400, once: true, offset: 300 });
+//     setTimeout(() => {
+//       AOS.refresh();
+//     }, 100);
+
+//     const autoScroll = () => {
+//       const scrollHeight = document.documentElement.scrollHeight;
+//       const scrollPosition = window.innerHeight + window.scrollY;
+
+//       if (scrollHeight === scrollPosition) {
+//         return;
+//       }
+//       window.scrollBy(0, 20);
+//       requestAnimationFrame(autoScroll);
+//     };
+//     autoScroll();
+//   }, []);
+
+//   return (
+//     <div className="space-y-5 pt-4 pb-10">
+//       <video
+//         className="w-full rounded-xs"
+//         autoPlay
+//         muted
+//         playsInline
+//         preload="auto"
+//       >
+//         <source src={data?.videos?.[0].url} type="video/mp4" />
+//         Your browser does not support the video tag.
+//       </video>
+//       <div className="space-y-3 px-4">
+//         <TitleInfo />
+//         <BreakingNews />
+//         <Bridegroom />
+//         <LoveStory />
+//         <OurGallery />
+//         {/* <Countdown />  */}
+//         <Location />
+//         <Gift />
+//         <WishSection />
+//       </div>
+//       <Footer />
+//       <SongButton />
+//     </div>
+//   );
+// }
