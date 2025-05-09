9c8d5c78329800ba31ddd10cccee98eeb60030b5
Noprizal <noprizal@voltras.co.id>
2025-05-09 16:49:26 +0700

    fix

--- hooks/use-local-storage.tsx
+++ hooks/use-local-storage.tsx
@@ -1,38 +1,32 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-type SetValue<T> = T | ((val: T) => T);
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 function useLocalStorage<T>(
   key: string,
   initialValue: T
-): [T, (value: SetValue<T>) => void] {
-  const [storedValue, setStoredValue] = useState<T>(initialValue);
+): [T | undefined, Dispatch<SetStateAction<T | undefined>>] {
+  const [storedValue, setStoredValue] = useState<T | undefined>(undefined);
 
   useEffect(() => {
     try {
       const item = window.localStorage.getItem(key);
-      if (item) {
-        setStoredValue(JSON.parse(item));
-      }
+      setStoredValue(item ? JSON.parse(item) : initialValue);
     } catch (error) {
       console.log("Error reading localStorage:", error);
+      setStoredValue(initialValue);
     }
-  }, [key]);
+  }, [key, initialValue]);
 
-  // useEffect to update local storage when the state changes
   useEffect(() => {
+    if (storedValue === undefined) return;
     try {
-      // Allow value to be a function so we have same API as useState
       const valueToStore =
         typeof storedValue === "function"
-          ? storedValue(storedValue)
+          ? (storedValue as (val: T | undefined) => T)(storedValue)
           : storedValue;
-      // Save state
       window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
-      // A more advanced implementation would handle the error case
       console.log(error);
     }
   }, [key, storedValue]);
