a9bdb2722b99f8cb4246a984928eeb344f646617
noprizal <rizalnov667@gmail.com>
2025-05-19 00:30:45 +0700

    fix

--- fonts/fonts.ts
+++ fonts/fonts.ts
@@ -1,4 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Roboto, Playfair_Display } from "next/font/google";
+import localFont from "next/font/local";
 
 // default font for the app
 export const geistSans = Geist({
@@ -10,4 +11,24 @@ export const geistSans = Geist({
 export const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
+});
+
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+  variable: "--font-playfair",
+  display: "swap",
+});
+
+export const optivaground = localFont({
+  src: "./optivaground/OPTIVagRound-Bold.otf",
+  variable: "--font-optivaground",
+  display: "swap",
 });
--- themes/floral/components/Hero.tsx
+++ themes/floral/components/Hero.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import { optivaground } from "@/fonts/fonts";
 import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
   const [isOpened, setIsOpened] = useState(false);
@@ -13,21 +16,33 @@ const Hero = () => {
     }, 1000);
   };
 
+  useEffect(() => {
+    if (!isOpened) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpened]);
+
   return (
     <AnimatePresence>
       {!isOpened && (
         <motion.div
           className="fixed inset-0 z-50 bg-cover bg-center flex items-center justify-center origin-top"
-          //   style={{
-          //     backgroundImage: `url('https://slametandfatma.wedding/galeri/1.jpg')`,
-          //   }}
+          style={{
+            backgroundImage: `url('https://slametandfatma.wedding/galeri/1.jpg')`,
+          }}
           initial={{ y: 0 }}
           animate={isAnimating ? { y: "-100%" } : { y: 0 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 1, ease: "easeInOut" }}
         >
           <section className="relative h-screen w-full max-w-sm mx-auto">
-            <video
+            {/* <video
               autoPlay
               muted
               loop
@@ -37,7 +52,7 @@ const Hero = () => {
                 src="https://slametandfatma.wedding/video/myvideo.mp4"
                 type="video/mp4"
               />
-            </video>
+            </video> */}
 
             <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-black/70 to-black/95 z-10 pointer-events-none" />
 
@@ -45,7 +60,11 @@ const Hero = () => {
               <h1 className="text-5xl font-bold font-serif">AB</h1>
               <div className="border border-red-500 flex flex-col items-center justify-center">
                 <p className="text-sm mb-2">Hello</p>
-                <p className="text-xl mb-6">Family and Friends</p>
+                <p
+                  className={`tracking-wider font-medium ${optivaground.className} text-3xl mb-6`}
+                >
+                  Family and Friends
+                </p>
                 <button
                   onClick={handleOpenInvitation}
                   className="mt-4 px-6 py-2 border border-white hover:bg-white hover:text-black transition duration-300"
