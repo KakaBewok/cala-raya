815dfdcf56b7ce958bdc37fd486185f55ec45736
noprizal <rizalnov667@gmail.com>
2025-05-21 00:43:01 +0700

    fix

--- themes/floral/components/Greetings.tsx
+++ themes/floral/components/Greetings.tsx
@@ -2,28 +2,38 @@ import Image from "next/image";
 
 export default function Greetings() {
   return (
-    <div className="h-screen bg-[#fdfaf6] flex flex-col items-center justify-center border border-red-500 relative overflow-hidden px-4">
+    <div className="h-screen bg-[#fdfaf6] flex flex-col items-center justify-center border border-red-500 relative overflow-hidden">
+      <Image
+        src={`/assets/images/bunga3.png`}
+        width={80}
+        height={80}
+        alt="Bunga kiri atas"
+        className="swing-left-fast absolute top-9 left-12"
+      />
+      <Image
+        src={`/assets/images/bunga2.png`}
+        width={80}
+        height={80}
+        alt="Bunga kiri atas"
+        className="swing-right-fast absolute top-[400px] left-10"
+      />
       {/* frame */}
       <div className="border border-[#e6d6c9] p-1 text-center max-w-md bg-white/60 backdrop-blur-md relative z-10">
         <div className="border border-[#e6d6c9] p-10 text-center max-w-md bg-white/60 backdrop-blur-md relative z-10">
-          {/* <AnimatedFlower
-            src="/assets/images/favicon.ico"
-            className="flower-animate w-20 h-20 absolute top-0 left-0"
-          /> */}
-          <Image
-            src={`/assets/images/bunga3.png`}
-            width={9}
-            height={9}
-            alt="Bunga kiri atas"
-            className="flower-animate-fast absolute top-0 -left-12 w-20 animate-floating border border-red-500"
-          />
           <Image
             src={`/assets/images/bunga4.png`}
-            width={9}
-            height={9}
+            width={100}
+            height={100}
             alt="Bunga kanan bawah"
-            className="flower-animate-fast absolute bottom-0 right-0 w-20 animate-floating-reverse"
+            className="swing-right-fast absolute -bottom-6 -right-6"
           />
+          {/* <Image
+            src={`/assets/images/bunga4.png`}
+            width={100}
+            height={100}
+            alt="Bunga kanan bawah"
+            className="swing-right-fast absolute -bottom-6 -left-6"
+          /> */}
           <h1 className="text-4xl font-serif text-[#c6a886] mb-6">• SF •</h1>
           <p className="text-xs text-[#c6a886] leading-relaxed font-medium">
             It has been eight long years.
@@ -47,10 +57,13 @@ export default function Greetings() {
         </div>
       </div>
 
-      {/* Tulisan Arab */}
-      <p className="mt-16 text-[#c6a886] text-xl font-arabic">
-        بسم الله الرحمن الرحيم
-      </p>
+      <div className="mt-20 w-full flex justify-center items-center bg-[#efe7dd] h-32">
+        <p className=" text-[#c1915a] text-xl font-arabic">
+          بسم الله الرحمن الرحيم
+        </p>
+      </div>
     </div>
   );
 }
+
+// text-[#c6a886]
