1917b039c45f1ba4bc5d0dbf1571ac4cc6d53e6f
Noprizal <noprizal@voltras.co.id>
2025-05-21 16:02:44 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "calaraya.vercel.app",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
 };
--- themes/floral/components/Greetings.tsx
+++ themes/floral/components/Greetings.tsx
@@ -3,37 +3,9 @@ import Image from "next/image";
 export default function Greetings() {
   return (
     <div className="h-screen bg-[#fdfaf6] flex flex-col items-center justify-center border border-red-500 relative overflow-hidden">
-      <Image
-        src={`/assets/images/bunga3.png`}
-        width={80}
-        height={80}
-        alt="Bunga kiri atas"
-        className="swing-left-fast absolute top-9 left-12"
-      />
-      <Image
-        src={`/assets/images/bunga2.png`}
-        width={80}
-        height={80}
-        alt="Bunga kiri atas"
-        className="swing-right-fast absolute top-[400px] left-10"
-      />
       {/* frame */}
       <div className="border border-[#e6d6c9] p-1 text-center max-w-md bg-white/60 backdrop-blur-md relative z-10">
         <div className="border border-[#e6d6c9] p-10 text-center max-w-md bg-white/60 backdrop-blur-md relative z-10">
-          <Image
-            src={`/assets/images/bunga4.png`}
-            width={100}
-            height={100}
-            alt="Bunga kanan bawah"
-            className="swing-right-fast absolute -bottom-6 -right-6"
-          />
-          {/* <Image
-            src={`/assets/images/bunga4.png`}
-            width={100}
-            height={100}
-            alt="Bunga kanan bawah"
-            className="swing-right-fast absolute -bottom-6 -left-6"
-          /> */}
           <h1 className="text-4xl font-serif text-[#c6a886] mb-6">• SF •</h1>
           <p className="text-xs text-[#c6a886] leading-relaxed font-medium">
             It has been eight long years.
@@ -57,7 +29,50 @@ export default function Greetings() {
         </div>
       </div>
 
-      <div className="mt-20 w-full flex justify-center items-center bg-[#efe7dd] h-32">
+      {/* floral */}
+      <Image
+        src={`/assets/images/floral/bunga7.webp`}
+        width={70}
+        height={70}
+        alt="Bunga kanan tengah"
+        className="swing-right-fast absolute top-56 right-12 z-20"
+      />
+      <Image
+        src={`/assets/images/floral/bunga6.webp`}
+        width={70}
+        height={70}
+        alt="Bunga kiri tengah"
+        className="swing-left-fast absolute top-56 left-10 z-20"
+      />
+      <Image
+        src={`/assets/images/floral/bunga3.webp`}
+        width={80}
+        height={80}
+        alt="Bunga kiri atas"
+        className="swing-left-fast absolute top-9 left-12"
+      />
+      <Image
+        src={`/assets/images/floral/bunga4.webp`}
+        width={110}
+        height={110}
+        alt="Bunga kanan atas"
+        className="swing-left-fast absolute top-16 right-12 z-20"
+      />
+      <Image
+        src={`/assets/images/floral/bunga2.webp`}
+        width={80}
+        height={80}
+        alt="Bunga kiri bawah"
+        className="swing-left-fast absolute bottom-48 left-14 z-10"
+      />
+      <Image
+        src={`/assets/images/floral/bunga5.webp`}
+        width={160}
+        height={160}
+        alt="Bunga kanan bawah"
+        className="swing-right-fast absolute bottom-[216px] right-6 z-10"
+      />
+      <div className="z-20 mt-20 w-full flex justify-center items-center bg-[#efe7dd] h-32">
         <p className=" text-[#c1915a] text-xl font-arabic">
           بسم الله الرحمن الرحيم
         </p>
