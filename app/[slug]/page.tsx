708fcfde5ebf0fac16ad861af2acf2dccaf4ec0d
noprizal <rizalnov667@gmail.com>
2025-07-02 14:11:22 +0700

    fix

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -8,7 +8,7 @@ import MagazinePage from "@/themes/magazine/MagazinePage";
 import MaroonPage from "@/themes/maroon/MaroonPage";
 import MonochromePage from "@/themes/monochrome/MonochromePage";
 import NetflixPage from "@/themes/netflix/NetflixPage";
-import StylishBoldPage from "@/themes/stylish-bold/StylishBoldPage";
+import StylishBoldPage from "@/themes/stylishBold/StylishBoldPage";
 import { ThemeName } from "@/types/theme-name";
 import { decode } from "@/utils/hash";
 import { useParams, useSearchParams } from "next/navigation";
