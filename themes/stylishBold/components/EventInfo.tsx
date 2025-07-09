27c80e04e28634c1ba9983a4bca83b40401d110d
Noprizal <noprizal@voltras.co.id>
2025-07-09 18:54:00 +0700

    fix

--- themes/stylishBold/components/EventInfo.tsx
+++ themes/stylishBold/components/EventInfo.tsx
@@ -1,9 +1,16 @@
+import { nyghtSerif, theSecret } from "@/fonts/fonts";
 import { useInvitation } from "@/hooks/use-invitation";
 import { Rundown } from "@/types/invitation-data";
+import { formatDate } from "@/utils/format-date";
+import { formatTime } from "@/utils/format-time";
 import { DateTime } from "luxon";
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
+import Link from "next/link";
+import { useRef } from "react";
 
 const EventInfo = () => {
   const { invitationData: data } = useInvitation();
+  const lineRef = useRef<HTMLDivElement | null>(null);
 
   const generateGoogleCalendarUrl = (rundown: Rundown): string => {
     const date = rundown.date;
@@ -36,82 +43,122 @@ const EventInfo = () => {
     return googleCalendarUrl;
   };
 
+  const { scrollYProgress: lineScrollY } = useScroll({
+    target: lineRef,
+    offset: ["start 70%", "start 50%"],
+  });
+
+  const rawLineHeight = useTransform(
+    lineScrollY,
+    [0, 0.5, 1],
+    ["0%", "50%", "100%"]
+  );
+
+  const lineHeight = useSpring(rawLineHeight, {
+    damping: 20,
+    stiffness: 100,
+  });
+
+  const eventDate = new Date(data?.event_date ?? "");
+  const day = String(eventDate.getDate()).padStart(2, "0");
+  const month = String(eventDate.getMonth() + 1).padStart(2, "0");
+  const year = String(eventDate.getFullYear()).slice(-2);
+
   return (
     <section className="relative w-full h-screen bg-[#ede0d1] overflow-hidden flex justify-center items-center">
       {/* LEFT SIDE */}
       <div className="flex h-full flex-col items-center justify-center pl-6 pr-4 flex-shrink-0 relative overflow-hidden">
-        <div className="flex flex-col items-center justify-center text-[64px] font-nyght-serif text-center leading-none opacity-100">
-          02
+        <div
+          className={`flex flex-col items-center justify-center text-[64px] ${nyghtSerif.className} text-center leading-none`}
+        >
+          {day}
           <br />
-          11
+          {month}
           <br />
-          24
+          {year}
         </div>
-        <span className="h-full border border-rose-900 absolute top-[88px] right-0 origin-top"></span>
+        <motion.div
+          ref={lineRef}
+          style={{ height: lineHeight }}
+          className=" border border-rose-700 absolute top-[88px] right-0 origin-top"
+        />
       </div>
       {/* RIGHT SIDE */}
-      <div className="flex-grow relative flex-col flex py-10 gap-6">
-        <div className="flex flex-col gap-7 pl-4 pr-6 py-5">
-          <div className="relative text-4xl text-ruby font-the-secret font-medium after:w-14 after:border-ruby after:border-t-[0.5px] after:absolute after:-bottom-3 after:left-0 after:h-0">
-            Akad Nikah
-          </div>
-          <div className="flex flex-col gap-2 font-nyght-serif text-raisin-black">
-            <div className="text-base font-normal">Sabtu, 02 November 2024</div>
-            <div className="text-base font-normal">08:00 10:00 WIB</div>
-            <div className="text-[10px]">
-              <div className="underline cursor-pointer">Tambah ke Kalender</div>
+      <div className="flex-grow relative flex-col flex gap-6">
+        {data?.rundowns?.map((rundown, index) => (
+          <div className="flex flex-col gap-7 pl-4 pr-6 py-5" key={index}>
+            <div
+              className={`${theSecret.className} text-rose-900 text-4xl font-medium`}
+            >
+              <p>{rundown.title}</p>
+              <div className="mt-2 w-14 h-[0.9px] bg-rose-900" />
+            </div>
+            <div
+              className={`${nyghtSerif.className} flex flex-col gap-2 text-neutral-800`}
+            >
+              <div className="text-base font-normal">
+                {formatDate(rundown.date, true)}
+              </div>
+              <div className="text-base font-normal">
+                {" "}
+                {formatTime(rundown.start_time)} -{" "}
+                {rundown.end_time && rundown.start_time
+                  ? `${formatTime(rundown.end_time)} ${rundown.time_zone}`
+                  : "selesai"}{" "}
+              </div>
+              <div className="text-[11px]">
+                <Link
+                  href={generateGoogleCalendarUrl(rundown)}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline cursor-pointer"
+                >
+                  Tambah ke Kalender
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-7 pl-4 pr-6 py-5">
-          <div className="relative text-4xl text-ruby font-the-secret font-medium after:w-14 after:border-ruby after:border-t-[0.5px] after:absolute after:-bottom-3 after:left-0 after:h-0">
-            Resepsi
+        ))}
+
+        <div className="flex flex-col gap-6 pl-4 pr-6 py-5">
+          <div
+            className={`${theSecret.className} text-4xl text-rose-800 font-medium`}
+          >
+            <span>Lokasi</span>
+            <div className="mt-3 w-14 h-[0.9px] bg-rose-900" />
           </div>
-          <div className="flex flex-col gap-2 font-nyght-serif text-raisin-black">
-            <div className="text-base font-normal">Sabtu, 02 November 2024</div>
-            <div className="text-base font-normal">11:00 14:00 WIB</div>
-            <div className="text-[10px]">
-              <div className="underline cursor-pointer">Tambah ke Kalender</div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-7 pl-4 pr-6 py-5">
-          <div className="relative text-4xl text-ruby font-the-secret font-medium after:w-14 after:border-ruby after:border-t-[0.5px] after:absolute after:-bottom-3 after:left-0 after:h-0">
-            Lokasi
-          </div>
-          <div className="flex flex-col gap-2 font-nyght-serif text-raisin-black mt-2">
-            <div className="text-base font-normal">Aulia Hall Center</div>
-            <div className="text-xs font-light italic">
-              Sukarindik, Bungursari, Tasikmalaya
-            </div>
-            <div className="mb-2">
-              <button
-                className="relative inline-flex items-center justify-center bg-transparent overflow-visible px-4 py-[10px] text-xs text-raisin-black font-medium"
-                type="button"
+          <div
+            className={`${nyghtSerif.className} flex flex-col gap-2 text-neutral-800 mt-2`}
+          >
+            <div className="text-base font-normal">{data?.location}</div>
+            <div className="cursor-pointer relative inline-flex items-center justify-center font-light overflow-visible px-4 py-[10px] text-xs w-40">
+              <div className="absolute inset-0 overflow-hidden w-40">
+                <svg
+                  className="absolute w-full h-full"
+                  preserveAspectRatio="none"
+                  viewBox="0 0 100 40"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <ellipse
+                    cx="50"
+                    cy="20"
+                    rx="49.5"
+                    ry="19.5"
+                    fill="none"
+                    stroke="#EF4444"
+                    strokeWidth="1"
+                    vectorEffect="non-scaling-stroke"
+                  />
+                </svg>
+              </div>
+              <Link
+                href={data?.location_url || "#"}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative text-neutral-800"
               >
-                <div className="absolute inset-0 overflow-hidden">
-                  <svg
-                    className="absolute w-full h-full stroke-jasper"
-                    preserveAspectRatio="none"
-                    viewBox="0 0 100 40"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <ellipse
-                      cx="50"
-                      cy="20"
-                      rx="49.5"
-                      ry="19.5"
-                      fill="none"
-                      vector-effect="non-scaling-stroke"
-                      stroke-width="1"
-                      stroke="jasper"
-                    ></ellipse>
-                  </svg>
-                </div>
-                <span className="relative z-10 font-nyght-serif">
-                  Lihat Lokasi
-                </span>
-              </button>
+                Lihat Lokasi
+              </Link>
             </div>
           </div>
         </div>
