9f000bef5ae8c5838fa931277da7f67e8b1a2405
Noprizal <noprizal@voltras.co.id>
2026-02-16 08:20:12 +0700

    fix

--- themes/magazine_custom/components/Bismillah.tsx
+++ themes/magazine_custom/components/Bismillah.tsx
@@ -2,21 +2,13 @@ import Image from "next/image";
 
 const Bismillah = () => {
   return (
-    // <div className="flex flex-col items-center justify-center gap-1 bg-white py-10 px-10 text-center">
-    //   <p className="text-xs text-gray-400 font-medium" data-aos="fade-up">
-    //    1 Peter 4:8 – “Above all, love each other deeply, because love covers over a multitude of sins.”
-    //   </p>
-    //   <p className="text-xs text-gray-400 font-medium" data-aos="fade-up">
-    //     We cordially invite you to attend our wedding
-    //   </p>
-    // </div>
     <div className="text-center max-w-lg mx-auto px-6 py-12">
     <div className="mb-8">
-      <p className="italic text-gray-400 leading-relaxed text-sm">
+      <p className="italic text-gray-400 leading-relaxed text-xs">
         "Above all, love each other deeply, because love covers over a multitude of sins."
       </p>
-      <p className="text-xs font-semibold uppercase tracking-widest text-gray-400 mt-2">
-        — 1 Peter 4:8
+      <p className="text-xs font-semibold tracking-widest text-gray-500 mt-2">
+        1 Peter 4:8
       </p>
     </div>
 
